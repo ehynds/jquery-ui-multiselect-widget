45b42d9541e456c3f8d41d09927fb79749893e3d
Eric Hynds <eric@hynds.net>
2010-09-17 16:03:38 -0400

    unit tests

--- tests/unit/options.js
+++ tests/unit/options.js
@@ -61,4 +61,98 @@
 		el.multiselect("destroy");
 	});
 
+	test("height", function(){
+		expect(2);
+		
+		var height = 234;
+		
+		el = $("select").multiselect({ height: height }).multiselect("open");
+		ok( height === widget().find("ul.ui-multiselect-checkboxes").height(), 'height after opening propertly set to '+height );
+		
+		// change height and re-test
+		height = 333;
+		el.multiselect("option", "height", height);
+		ok( height === widget().find("ul.ui-multiselect-checkboxes").height(), 'changing value through api to '+height );
+		
+		el.multiselect("destroy");
+	});
+
+	test("minWidth", function(){
+		expect(3);
+		
+		var minWidth = 321;
+		
+		el = $("select").multiselect({ minWidth:minWidth }).multiselect("open");
+		ok( minWidth === button().outerWidth(), 'outerWidth of button is '+minWidth );
+		
+		// change height and re-test
+		minWidth = 351;
+		el.multiselect("option", "minWidth", minWidth);
+		ok( minWidth === button().outerWidth(), 'changing value through api to '+minWidth);
+		
+		// change height to something that should fail.
+		minWidth = 10;
+		el.multiselect("option", "minWidth", minWidth);
+		var outerWidth = button().outerWidth();
+		ok( minWidth !== outerWidth, 'changing value through api to '+minWidth+' (too small), outerWidth is actually ' + outerWidth);
+		
+		el.multiselect("destroy");
+	});
+
+	test("checkAllText", function(){
+		expect(2);
+		var text = "foo";
+		
+		el = $("select").multiselect({ checkAllText:text });
+		ok( text === widget().find(".ui-multiselect-all").text(), 'check all link reads '+text );
+		
+		// set through option
+		text = "bar";
+		el.multiselect("option","checkAllText","bar");
+		ok( text === widget().find(".ui-multiselect-all").text(), 'check all link reads '+text );
+		
+		el.multiselect("destroy");
+	});
+
+	test("uncheckAllText", function(){
+		expect(2);
+		var text = "foo";
+		
+		el = $("select").multiselect({ uncheckAllText:text });
+		ok( text === widget().find(".ui-multiselect-none").text(), 'check all link reads '+text );
+		
+		// set through option
+		text = "bar";
+		el.multiselect("option","uncheckAllText","bar");
+		ok( text === widget().find(".ui-multiselect-none").text(), 'changing value through api to '+text );
+		
+		el.multiselect("destroy");
+	});
+
+	test("autoOpen", function(){
+		expect(2);
+		
+		el = $("select").multiselect({ autoOpen:false });
+		ok( menu().is(":hidden"), 'menu is hidden with autoOpen off');
+		el.multiselect("destroy");
+		
+		el = $("select").multiselect({ autoOpen:true });
+		ok( menu().is(":visible"), 'menu is visible with autoOpen on');
+		el.multiselect("destroy");
+		
+		// no built in support for change on the fly; not testing it.
+	});
+	
+	test("multiple", function(){
+		expect(2);
+		
+		el = $("select").multiselect({ multiple:false });
+		ok( !widget().find(":checkbox").length, 'no checkboxes are present');
+		ok( widget().find(":radio").length > 0, 'but radio boxes are');
+		
+		// not testing change on the fly here - IE doesn't support that.
+		
+		el.multiselect("destroy");
+	});
+	
 })(jQuery);
