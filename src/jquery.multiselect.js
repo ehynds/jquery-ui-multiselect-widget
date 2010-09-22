098730d30c7d8d6f88d226d1f0b9118c880b1fb4
Eric Hynds <eric@hynds.net>
2010-09-22 14:58:21 -0400

    1.4.1

--- src/jquery.multiselect.js
+++ src/jquery.multiselect.js
@@ -1,5 +1,5 @@
 /*
- * jQuery MultiSelect UI Widget 1.4.1pre
+ * jQuery MultiSelect UI Widget 1.4.1
  * Copyright (c) 2010 Eric Hynds
  *
  * http://www.erichynds.com/jquery/jquery-ui-multiselect-widget/
