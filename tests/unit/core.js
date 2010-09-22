f33086da5c409d7944543f21bcfd0b2c52ef9b4f
Eric Hynds <eric@hynds.net>
2010-09-22 14:38:47 -0400

    form submission unit tests

--- tests/unit/core.js
+++ tests/unit/core.js
@@ -12,7 +12,6 @@ function menu(){
 	return button().next();
 }
 
-
 (function($){
 
 	module("core");
@@ -23,6 +22,30 @@ function menu(){
 		el = $("select").multiselect();
 			ok( el.is(":hidden"), 'Original select is hidden');
 		el.multiselect("destroy");
+	});
+
+	test("form submission", function(){
+		expect(3);
+		
+		var form = $('<form></form>'), data;
+		
+		el = $('<select name="test" multiple="multiple"><option value="foo" selected="selected">foo</option><option value="bar">bar</option></select>')
+			.appendTo(form)
+			.multiselect()
+			.multiselect("checkAll");
+			
+		// this test ensures that the underlying option tags weren't serialized as well
+		data = form.serialize();
+		ok( data === 'test=foo&test=bar', 'after checking all and serializing the form, the correct keys were serialized: ' + data);
+		
+		el.multiselect("uncheckAll");
+		data = form.serialize();
+		ok( !data.length, 'after unchecking all and serializing the form, nothing was serialized: ' + data);
+		
+		// re-check all and destroy, exposing original select
+		el.multiselect("checkAll").multiselect("destroy");
+		data = form.serialize();
+		ok( data === 'test=foo&test=bar', 'after checking all, destroying the widget, and serializing the form, the correct keys were serialized: ' + data);
 	});
 	
 })(jQuery);
