d2bbe2b9852f51ab57c7cfc1d8f2f2248520b008
Eric Hynds <eric@hynds.net>
2010-09-30 18:42:03 -0400

    single select unit tests

--- tests/unit/options.js
+++ tests/unit/options.js
@@ -13,25 +13,25 @@
 		// read from widget
 		text = el.multiselect("option", "noneSelectedText");
 		
-		ok( button().text() === text, 'on init, button reads "None Selected"');
+		equals( button().text(), text, 'on init, button reads "None Selected"');
 		el.multiselect("checkAll");
 		ok( button().text() !== text, 'after checkAll, button no longer reads "None Selected"');
 		el.multiselect("uncheckAll");
-		ok( button().text() === text, 'after uncheckAll, button text restored to "None Selected"');
+		equals( button().text(), text, 'after uncheckAll, button text restored to "None Selected"');
 		
 		// change the option value
 		el.multiselect("option", "noneSelectedText", "No Checkboxes Checked");
-		ok( el.multiselect("option", "noneSelectedText") === "No Checkboxes Checked", "new noneSelectedText value set correctly");
+		equals( el.multiselect("option", "noneSelectedText"), "No Checkboxes Checked", "new noneSelectedText value set correctly");
 		
 		// read updated value from widget
 		text = el.multiselect("option", "noneSelectedText");
 		
 		// test against the new value
-		ok( button().text() === text, 'after changing the option value, button now reads "No Checkboxes Checked"');
+		equals( button().text(), text, 'after changing the option value, button now reads "No Checkboxes Checked"');
 		el.multiselect("checkAll");
 		ok( button().text() !== text, 'after checkAll, button no longer reads "No Checkboxes Checked"');
 		el.multiselect("uncheckAll");
-		ok( button().text() === text, 'after uncheckAll, button text restored to "No Checkboxes Checked"');
+		equals( button().text(), text, 'after uncheckAll, button text restored to "No Checkboxes Checked"');
 		
 		el.multiselect("destroy");
 	});
@@ -45,18 +45,18 @@
 		});
 		
 		el.multiselect("checkAll");
-		ok( button().text() === numOptions+' of '+numOptions+' selected', 'after checkAll, button reflects the total number of checked boxes');
+		equals( button().text(), numOptions+' of '+numOptions+' selected', 'after checkAll, button reflects the total number of checked boxes');
 		
 		// change option value
 		el.multiselect("option","selectedText", function( numChecked ){
 			return numChecked + ' options selected';
 		});
 		
-		ok( button().text() === numOptions+' options selected', 'after changing the option to a function value, button reflects the new text');
+		equals( button().text(), numOptions+' options selected', 'after changing the option to a function value, button reflects the new text');
 		
 		// uncheck all
 		el.multiselect("uncheckAll");
-		ok( button().text() === el.multiselect("option","noneSelectedText"), 'after unchecking all, button text now reflects noneSelectedText option value');
+		equals( button().text(), el.multiselect("option","noneSelectedText"), 'after unchecking all, button text now reflects noneSelectedText option value');
 		
 		el.multiselect("destroy");
 	});
@@ -67,12 +67,12 @@
 		var height = 234;
 		
 		el = $("select").multiselect({ height: height }).multiselect("open");
-		ok( height === widget().find("ul.ui-multiselect-checkboxes").height(), 'height after opening propertly set to '+height );
+		equals( height, widget().find("ul.ui-multiselect-checkboxes").height(), 'height after opening propertly set to '+height );
 		
 		// change height and re-test
 		height = 333;
 		el.multiselect("option", "height", height);
-		ok( height === widget().find("ul.ui-multiselect-checkboxes").height(), 'changing value through api to '+height );
+		equals( height, widget().find("ul.ui-multiselect-checkboxes").height(), 'changing value through api to '+height );
 		
 		el.multiselect("destroy");
 	});
@@ -83,12 +83,12 @@
 		var minWidth = 321;
 		
 		el = $("select").multiselect({ minWidth:minWidth }).multiselect("open");
-		ok( minWidth === button().outerWidth(), 'outerWidth of button is '+minWidth );
+		equals( minWidth, button().outerWidth(), 'outerWidth of button is '+minWidth );
 		
 		// change height and re-test
 		minWidth = 351;
 		el.multiselect("option", "minWidth", minWidth);
-		ok( minWidth === button().outerWidth(), 'changing value through api to '+minWidth);
+		equals( minWidth, button().outerWidth(), 'changing value through api to '+minWidth);
 		
 		// change height to something that should fail.
 		minWidth = 10;
@@ -104,12 +104,12 @@
 		var text = "foo";
 		
 		el = $("select").multiselect({ checkAllText:text });
-		ok( text === widget().find(".ui-multiselect-all").text(), 'check all link reads '+text );
+		equals( text, widget().find(".ui-multiselect-all").text(), 'check all link reads '+text );
 		
 		// set through option
 		text = "bar";
 		el.multiselect("option","checkAllText","bar");
-		ok( text === widget().find(".ui-multiselect-all").text(), 'check all link reads '+text );
+		equals( text, widget().find(".ui-multiselect-all").text(), 'check all link reads '+text );
 		
 		el.multiselect("destroy");
 	});
@@ -119,12 +119,12 @@
 		var text = "foo";
 		
 		el = $("select").multiselect({ uncheckAllText:text });
-		ok( text === widget().find(".ui-multiselect-none").text(), 'check all link reads '+text );
+		equals( text, widget().find(".ui-multiselect-none").text(), 'check all link reads '+text );
 		
 		// set through option
 		text = "bar";
 		el.multiselect("option","uncheckAllText","bar");
-		ok( text === widget().find(".ui-multiselect-none").text(), 'changing value through api to '+text );
+		equals( text, widget().find(".ui-multiselect-none").text(), 'changing value through api to '+text );
 		
 		el.multiselect("destroy");
 	});
@@ -144,11 +144,17 @@
 	});
 	
 	test("multiple", function(){
-		expect(2);
+		expect(3);
 		
 		el = $("select").multiselect({ multiple:false });
 		ok( !widget().find(":checkbox").length, 'no checkboxes are present');
 		ok( widget().find(":radio").length > 0, 'but radio boxes are');
+		
+		// simulate click on ALL radios
+		var radios = widget().find("input:radio").trigger("click");
+		
+		// at the end of that, only one radio should be selected
+		equals( radios.filter(":checked").length, 1, 'After checking all radios, only one is actually selected');
 		
 		// not testing change on the fly here - IE doesn't support that.
 		
