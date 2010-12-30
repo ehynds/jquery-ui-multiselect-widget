e95f97885df39eae3d4fe26ab8194923e17f5b1c
ehynds <eric@hynds.net>
2010-12-30 15:12:42 -0500

    added some localizations.  thanks, Vinicius!

---
+++ i18n/jquery.multiselect.es.js
@@ -0,0 +1,9 @@
+/* Spanish initialisation for the jQuery UI multiselect plugin. */
+/* Written by Vinícius Fontoura Corrêa (vinusfc@gmail.com). */
+
+$.extend($.ech.multiselect.prototype.options, {
+	checkAllText: 'Marca todas',
+	uncheckAllText: 'Desmarque todas',
+	noneSelectedText: 'Seleccione las opciones',
+	selectedText: '# seleccionado'
+});
