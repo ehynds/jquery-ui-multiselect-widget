fdc649f0193b0630a06e4c8f49ea881bf412ceb0
ehynds <eric@hynds.net>
2011-05-10 18:50:16 -0400

    remove pre

--- src/jquery.multiselect.js
+++ src/jquery.multiselect.js
@@ -1,5 +1,5 @@
 /*
- * jQuery MultiSelect UI Widget 1.10pre
+ * jQuery MultiSelect UI Widget 1.10
  * Copyright (c) 2011 Eric Hynds
  *
  * http://www.erichynds.com/jquery/jquery-ui-multiselect-widget/
