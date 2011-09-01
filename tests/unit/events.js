692137afc53a410cb3319c2f3a3be086feedb0d2
ehynds <eric@hynds.net>
2011-09-01 19:14:22 -0400

    fix broken change event test. #124

--- tests/unit/events.js
+++ tests/unit/events.js
@@ -242,18 +242,20 @@
 	
 	
 	test("multiselectbeforeoptgrouptoggle", function(){
-		expect(11);
+		expect(10);
 		
 		// inject widget
 		el = $('<select multiple><optgroup label="Set One"><option value="1">Option 1</option><option value="2">Option 2</option></optgroup></select>').appendTo(body);
 
-		el.multiselect({
+		el.bind("change", function(){
+			ok(true, "the select's change event fires");
+		})
+		.multiselect({
 			beforeoptgrouptoggle: function(e,ui){
 				equals(this, el[0], "option: context of callback");
 				equals(e.type, 'multiselectbeforeoptgrouptoggle', 'option: event type in callback');
 				equals(ui.label, "Set One", 'option: ui.label equals');
 				equals(ui.inputs.length, 2, 'option: number of inputs in the ui.inputs key');
-				ok(true, "option: the select's change event fires");
 			}
 		})
 		.bind("multiselectbeforeoptgrouptoggle", function(e,ui){
@@ -261,7 +263,6 @@
 			equals(this, el[0], 'event: context of event');
 			equals(ui.label, "Set One", 'event: ui.label equals');
 			equals(ui.inputs.length, 2, 'event: number of inputs in the ui.inputs key');
-			ok(true, "event: the select's change event fires");
 		})
 		.multiselect("open");
 		
@@ -270,7 +271,10 @@
 		el.multiselect("destroy").remove();
 		
 		// test return false preventing checkboxes from activating
-		el.multiselect({
+		el.bind("change", function(){
+			// this should not fire.
+			ok( true );
+		}).multiselect({
 			beforeoptgrouptoggle: function(){
 				return false;
 			},
