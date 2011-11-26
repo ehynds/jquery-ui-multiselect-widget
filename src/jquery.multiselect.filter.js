19aa2b0938b11f2bc1293b37733ad239c549ebef
ehynds <eric@hynds.net>
2011-11-26 10:31:43 -0500

    bump filter version

--- src/jquery.multiselect.filter.js
+++ src/jquery.multiselect.filter.js
@@ -1,5 +1,5 @@
 /*
- * jQuery MultiSelect UI Widget Filtering Plugin 1.3
+ * jQuery MultiSelect UI Widget Filtering Plugin 1.4pre
  * Copyright (c) 2011 Eric Hynds
  *
  * http://www.erichynds.com/jquery/jquery-ui-multiselect-widget/
