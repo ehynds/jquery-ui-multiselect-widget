780c2b4e0438d018dcc0875e9af43a72e20afeca
Charles SANQUER <charles.sanquer@spyrit.net>
2012-01-01 23:56:37 +0100

    add french translation

---
+++ i18n/jquery.multiselect.filter.fr.js
@@ -0,0 +1,7 @@
+/* French initialisation for the jQuery UI multiselect plugin. */
+/* Written by Charles SANQUER (charles.sanquer@spyrit.net). */
+
+$.extend($.ech.multiselectfilter.prototype.options, {
+	label: "Filtre:",
+	placeholder: "Entrer un mot clé"
+});
