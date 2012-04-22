e5b8fe7efa031411b5b393110442361a13595af1
Mastropinguino <mastropinguino@networky.net>
2012-04-22 09:29:23 +0200

    Added italian translations

---
+++ i18n/jquery.multiselect.filter.it.js
@@ -0,0 +1,11 @@
+/* French initialisation for the jQuery UI multiselect plugin. */
+/* Written by Charles SANQUER (charles.sanquer@spyrit.net). */
+
+(function ( $ ) {
+
+$.extend($.ech.multiselectfilter.prototype.options, {
+	label: "Filtro:",
+	placeholder: "Digita una parola chiave"
+});
+
+})( jQuery );
