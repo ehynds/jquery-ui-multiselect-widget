3195cf0534b7223427f16757e5c39cafc52187a5
ehynds <eric@hynds.net>
2012-10-23 13:25:29 -0400

    bump version

--- src/jquery.multiselect.js
+++ src/jquery.multiselect.js
@@ -1,6 +1,6 @@
 /* jshint forin:true, noarg:true, noempty:true, eqeqeq:true, boss:true, undef:true, curly:true, browser:true, jquery:true */
 /*
- * jQuery MultiSelect UI Widget 1.13
+ * jQuery MultiSelect UI Widget 1.14pre
  * Copyright (c) 2012 Eric Hynds
  *
  * http://www.erichynds.com/jquery/jquery-ui-multiselect-widget/
