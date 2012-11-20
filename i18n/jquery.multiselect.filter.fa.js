b69662c8ea56b32e4a4576ec5930b4a5695e38b0
Ghasem Shahabi <ghasem.shahabi@gmail.com>
2012-11-20 19:43:10 +0330

    Added RTL support CSS files and Persian(Farsi) i18n
    To support Right-to-Left, I'm just including the rtl css file after main
    css files to overwrite the Left-to-Right css(default), but it'd be
    better if we have an rtl option in the widget. it's just working fine ;)

---
+++ i18n/jquery.multiselect.filter.fa.js
@@ -0,0 +1,11 @@
+/* Persian(Farsi) initialisation for the jQuery UI multiselect plugin. */
+/* Written by Ghasem Shahabi (ghasem.shahabi@gmail.com). */
+
+(function ( $ ) {
+
+$.extend($.ech.multiselectfilter.prototype.options, {
+  label: "فيلتر:",
+  placeholder: "كلمات كليدي"
+});
+
+})( jQuery );
