4df10bca432b39b150361dab20dee7e5cbd23337
János Orcsik <orcsikj@gmail.com>
2015-09-23 13:09:25 +0200

    Add Hungarian translation

---
+++ i18n/jquery.multiselect.hu.js
@@ -0,0 +1,13 @@
+﻿/* Hungarian initialisation for the jQuery UI multiselect plugin. */
+/* Written by János Orcsik (orcsikj@gmail.com). */
+
+(function ($) {
+
+    $.extend($.ech.multiselect.prototype.options, {
+        checkAllText: 'Összes kiválasztása',
+        uncheckAllText: 'Kiválasztás törlése',
+        noneSelectedText: 'Válasszon',
+        selectedText: '# kiválasztva'
+    });
+
+})(jQuery);
