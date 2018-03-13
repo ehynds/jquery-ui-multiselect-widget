2dd53a96fef778f3a625348822028853df59f710
Brenda <brendahellenthal@gmail.com>
2018-03-13 16:39:15 +0100

    Prevent unwanted hiding of optgroups after refresh
    Fix same bug as was reported in issue #154.
    This original bug was fixed in commit 47854a0 (fix for hiding/showing optgroups when optionlist is hidden) but reintroduced in commit a9fcf40 (Option Groups are now ul elements instead of additional list items. )

--- src/jquery.multiselect.filter.js
+++ src/jquery.multiselect.filter.js
@@ -175,7 +175,9 @@
       // show/hide optgroups
       $groups.each(function() {
         var $this = $(this);
-        if(!$this.children("li:visible").length) {
+        // check with a function on display:none css instead of using :visible selector because newly created
+        // (on refresh) items are by default not (yet) visible but not hidden on purpose with the display:none.
+        if(!$this.children('li').filter(function () { return $.css(this, "display") !== 'none' }).length) {
           $this.hide();
         }
       });
